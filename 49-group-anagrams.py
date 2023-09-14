f18288a9d7c8a8b55dd4d2fc9cea3ad00e050371
allentsangdev <allentsang04@gmail.com>
2023-09-14 00:12:18 -0400

    leetcode question 49

--- 49-group-anagrams.py
+++ 49-group-anagrams.py
@@ -7,14 +7,15 @@ class Solution:
         hashMap = {}
         groupNumber = 1
         for i in strs:
-            key = str(sorted(i))
+            key = "".join(sorted(i))
             if i not in hashMap:
-                hashMap[i] = groupNumber
+                hashMap[key] = groupNumber
                 groupNumber += 1
         
         result = []
         for i in range(groupNumber+1):
             result.append([])
         for i in strs:
-            result[hashMap[str(sorted(i))]].append(i)
+            key = "".join(sorted(i))
+            result[hashMap[key]].append(i)
         return result
