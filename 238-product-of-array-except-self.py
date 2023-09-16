39e4d6cc5c229209d0150903b91833532f8799e5
allentsangdev <allentsang04@gmail.com>
2023-09-15 22:13:17 -0400

    question 238

---
+++ 238-product-of-array-except-self.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = []
+        prefix = []
+        suffix = []
+        
+        count = 1
+        for i in nums:
+            prefix.append(i * count)
+            count = i * count
+
+        count = 1
+        for i in range(len(nums)-1, -1 , -1):
+            suffix.append(nums[i] * count)
+            count = nums[i] * count
+
+        suffix.reverse()
+
+        for i in range(len(nums)):    
+            if i - 1 == -1:
+                answer.append(suffix[i+1])
+            elif i + 1 == len(nums):
+                answer.append(prefix[i-1])
+            else:
+                answer.append(prefix[i-1] * suffix[i + 1])
+        
+        print(prefix,suffix)
+        return answer
+    
+# Testing
+numberList = [1,2,3,4]
+
+soultion = Solution()
+print(soultion.productExceptSelf(numberList))
+
+
