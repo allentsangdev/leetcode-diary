1a55054f64250d7f3b602adce0c727a4f5b1883c
allentsangdev <allentsang04@gmail.com>
2023-09-26 20:31:49 -0400

    question 66

--- 66-plus-one.py
+++ 66-plus-one.py
@@ -2,6 +2,20 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1 ):
+            if i == 0 and digits[i] == 9:
+                digits[i] = 0
+                digits.insert(0, 1)
+                break
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                break
+
+        return digits
+            
+    def plusOne_lessEfficient(self, digits: List[int]) -> List[int]:
         number = ''
         for i in digits:
             number = number + str(i)
