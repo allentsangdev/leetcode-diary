f3c7b3eb6a903e51b3ab4ad77867977ce60f30c7
allentsangdev <allentsang04@gmail.com>
2023-09-29 01:46:49 -0400

    155-min-stack.py

---
+++ 155-min-stack.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack or val < self.minStack[-1]:
+            self.minStack.append(val) 
+        else:
+            self.minStack.append(self.minStack[-1])
+
+        self.stack.append(val)
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.minStack[-1]
