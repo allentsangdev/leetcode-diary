bede784d847f23cead7e125432fa9152aa215758
allentsangdev <allentsang04@gmail.com>
2024-01-10 18:10:41 -0500

    q238

--- 238-product-of-array-except-self.py
+++ 238-product-of-array-except-self.py
@@ -28,6 +28,37 @@ class Solution:
         
         print(prefix,suffix)
         return answer
+
+    from typing import List
+
+class Solution2:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = []
+        prefix = []
+        postfix = []
+
+        # fill in the prefix list
+        pre = 1
+        for i in range(len(nums)):
+            pre *= nums[i]
+            prefix.append(pre)
+            
+        # fill in the postfix list
+        post = 1
+        for i in range(len(nums)-1, -1, -1):
+            postfix.append(post * nums[i])
+            post *= nums[i]
+        postfix.reverse()
+                    
+        for i in range(len(nums)):
+            if i == 0:
+                res.append(postfix[i+1])
+            elif i == len(nums)-1:
+                res.append(prefix[i-1])
+            else:
+                res.append(prefix[i-1] * postfix[i+1])
+        return res
+
     
 # Testing
 numberList = [1,2,3,4]
